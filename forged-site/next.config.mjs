13472101def96f12095fc2764e8b6ef68d0b2bc8
beforged26 <pri@beforged.co>
2026-06-22 22:46:48 -0400

    Update next.config.mjs

--- forged-site/next.config.mjs
+++ forged-site/next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dbukjj6eu5tsf.cloudfront.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "fiusports.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "dxbhsrqyrr690.cloudfront.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
