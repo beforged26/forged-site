0286f6ab09c000c88b3be0d1c601f80a09053ae8
Optimum Volleyball Admin Beach <admin@iMac.home>
2026-06-26 20:38:14 -0400

    fix duplicate nextConfig declaration

--- forged-site/next.config.mjs
+++ forged-site/next.config.mjs
@@ -23,28 +23,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "dbukjj6eu5tsf.cloudfront.net",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "fiusports.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dxbhsrqyrr690.cloudfront.net",
-        pathname: "/**",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
